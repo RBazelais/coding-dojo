d02e0eed59e70500b179032583a737591bd445d7
rbazelais <bazelais.rachel@gmail.com>
2017-12-15 09:19:52 -0800

    algorithm practice

---
+++ Algorithms/Week1/Thurs/Reverse.js
@@ -0,0 +1,9 @@
+function Reverse(arr){
+    var newArr=[];
+    for (var i = arr.length-1; i >= 0; i--){
+        newArr[i] = arr[i];
+        console.log(arr[i]);
+    }
+    console.log(newArr[i]);
+}
+Reverse(['a','b','c']);
